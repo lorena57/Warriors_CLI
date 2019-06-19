578b038bc043689ee88d5779ea32a814d589accd
github username <github email address>
2019-06-18 21:05:35 -0700

    small updates to CLI

--- lib/warriors/cli.rb
+++ lib/warriors/cli.rb
@@ -2,40 +2,39 @@ class CLI
 
     def run
         intro
-        
         WarriorScraper.scrape_team
-
         list_players
+        previous_teams
+        guess_position
     end
 
     def intro  
-        puts "\n\nThis project is pissing me off!\n\n\n"
+        puts "\n\nWelcome to the Warriors information page!\n\n\n"
 
-        puts "2019 Warriors Players:\n\n\n"
+        puts "Let's read more about our 2019 Warriors Players:\n\n\n"
     end    
 
     def list_players
-        
         # Team.all.each.with_index(1){| p, i | puts "#{i}. #{p.team_player}"}
         Team.all.map do |team|
             team.team_player.each.with_index(1){| p, i | puts "#{i}. #{p}"}     
         end
-    end    
-
+    end  
+    
     def guess_position
-        puts "Hi my name is #{team_player[1]}, do you know what position do I play?"
-            # user will have a selection of different positions to choose from
-            # user will have 3 chances
-            # if user correct a comment of congratulations will populate
-            #play again? or go to main menu
-    end    
+        puts "Hi my name is #{team_player[1]}, and I'm a #{team_position} for the Golden State Warriors."
+    end   
 
     def previous_teams 
         #if yes, (second level)
-        puts "Do you want to know what other teams I've played for?"
+        puts "\n\nWould you like to know what other teams I've played for?\n\n"
+        puts "If so, please enter a number of which player you'd like to learn more about.\n\n"
         #allows user to declare yes or no if yes show list or if no return back
         #to main menu
-        
-    end    
+    end
+
+   
+
+    
 
 end    
